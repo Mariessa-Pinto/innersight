a1a019e73647412bed09c00abc8f6b3126b6a08c
pjiang604 <patriciaejiang@gmail.com>
2023-10-06 21:04:37 -0700

    Added app-wide dark mode. Testing font-size code.

--- screens/Home.js
+++ screens/Home.js
@@ -1,36 +1,31 @@
+import styles from '../styles/global'
+
+import { useState, useContext } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View, Button } from 'react-native';
-import { useState } from 'react';
-import Slider from '@react-native-community/slider';
-import styles from '../styles/global'
+import themeContext from '../theme/themeContext';
+
+
 
 
 export default function Home({ navigation }) {
 
     //Dark/Light Mode
-    const [colorScheme, setColorTheme] = useState("dark")
-    const [userFontSize, setUserFontSize] = useState(1)
-    const [sliderTracker, setSliderTracker] = useState(0)
-
-    const themeTextStyle = colorScheme === 'light' ? styles.lightThemeText : styles.darkThemeText;
-    const themeContainerStyle =
-        colorScheme === 'light' ? styles.lightContainer : styles.darkContainer;
-
+    const theme = useContext(themeContext)
 
     return (
-        <View style={[styles.container, themeContainerStyle]}>
-            <Text style={[styles.text, themeTextStyle]}>Welcome!</Text>
+        <View style={[styles.container, { backgroundColor: theme.backgroundColor }]}>
+            <Text style={[styles.text, { color: theme.color }]}>Welcome to the Home Page</Text>
 
             <StatusBar style="auto" />
             <Button
                 title="Go to about page"
                 onPress={() => navigation.push('About')}
             />
-            <Button title="Dark/Light Mode" onPress={() => colorScheme == "light" ? setColorTheme("dark") : setColorTheme("light")} />
-            <Text style={[styles.text, themeTextStyle]}>Change the font size (I don't work)</Text>
-            <Slider style={styles.slider} minimumTrackTintColor='#ffffff' maximumTrackTintColor='#ADD8E6' lowerLimit={0} upperLimit={5}
-                defaultValue={sliderTracker}
-                key={sliderTracker} />
+            <Button
+                title="Go to settings page"
+                onPress={() => navigation.push('Settings')}
+            />
         </View>
     );
 }
