97697c43dbbdcd580534f89d3de507899ed79299
bguemos <bri-lynn@hotmail.com>
2023-10-09 14:38:40 -0700

    Crisis and reccomenndation Buttons

---
+++ Atoms/JournalCovers/JournalCovers.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { Image } from "expo-image";
+
+
+export default function JournalCovers() {
+
+
+    return(
+
+        <View>
+          
+       
+     
+         
+
+
+        </View>
+    )
+}
