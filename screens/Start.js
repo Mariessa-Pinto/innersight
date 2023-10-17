facfecf518912925b5a8a15462b3bcfbff25b5a9
Mariessa-Pinto <mpinto13@my.bcit.ca>
2023-10-17 12:13:25 -0700

    Added start page elements

--- screens/Start.js
+++ screens/Start.js
@@ -1,11 +1,10 @@
 import globalStyles from '../styles/global'
-import { StyleSheet, Text, View, Button, Switch } from 'react-native';
+import { StyleSheet, Text, View, Button, Switch, ScrollView, TouchableOpacity } from 'react-native';
 import { useState, useContext } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import themeContext from '../theme/themeContext';
-import NavBar from '../molecules/NavBar';
-
-import { ScrollView } from 'react-native';
+import { Image } from 'expo-image';
+import ExtraLargeBtnLightTxt from '../atom/Buttons/ExtraLargeBtnLightTxt'
 
 
 
@@ -16,42 +15,50 @@ export default function Start({ navigation }) {
     const theme = useContext(themeContext)
 
 
+
     return (
         <>
             <View style={[globalStyles.container, { backgroundColor: theme.backgroundColor }]}>
+                <View style={styles.container}>
                     <StatusBar style="auto" />
-                    <Button
-                        title="Get Started"
+                    <Image
+                        source={require('../atom/icons/logo.png')}
+                        style={styles.logo}
+                    />
+                    <Image
+                        source={require('../atom/icons/logo_text.png')}
+                        style={styles.wordmark}
+                    />
+                    <ExtraLargeBtnLightTxt text="Get Started" 
                         onPress={() => navigation.push('Demo')}
                     />
-                    <Text>Hello</Text>
-                    <Text>Hello</Text>
-                    <Text>Hello</Text>
-                    <Text>Hello</Text>
-                    <Text>Hello</Text>
-                    <Text>Hello</Text>
+                </View>
             </View>
-            <NavBar navigation={navigation}/>
             </>
     );
 }
 
 const styles = StyleSheet.create({
-
-})
-
-
-const startStyles = StyleSheet.create({
     container: {
         backgroundColor: "#D5D7FF",
         display: 'flex',
         height: "100%",
         flexDirection: 'column',
         alignItems: 'center',
-        justifyContent: 'space-around',
+        justifyContent: 'center',
     },
     button: {
         borderRadius: 10,
         backgroundColor: "#6164C3",
+    },
+    logo: {
+        height: 150,
+        width: 150,
+        marginBottom: 20
+    },
+    wordmark: {
+        width: 300,
+        height: 60,
+        marginBottom: 42
     }
-});
+})
