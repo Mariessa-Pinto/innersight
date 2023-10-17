0e3f6da0570b25b9780fe58ec6df432af4bc34d7
bguemos <bri-lynn@hotmail.com>
2023-10-17 11:04:39 -0700

    commiting changes

--- screens/Stats.js
+++ screens/Stats.js
@@ -1,5 +1,5 @@
-import styles from '../styles/global'
-import { Text, View, Button, Switch } from 'react-native';
+import globalStyless from '../styles/global'
+import { Text, View, Button, Switch, StyleSheet } from 'react-native';
 import { useState, useContext } from 'react';
 import themeContext from '../theme/themeContext';
 
@@ -12,8 +12,19 @@ export default function Stats({ navigation }) {
 
 
     return (
-        <View style={[styles.container, { backgroundColor: theme.backgroundColor, marginTop: 100 }]}>
+        <View style={[globalStyles.container, { backgroundColor: theme.backgroundColor, marginTop: 100 }]}>
+            <View style={style.maincontent}>
+                <Text>Your Week In Review</Text>
+
+            </View>
 
         </View>
     );
 }
+const styles = StyleSheet.create({
+   maincontent: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between'
+   }
+})
