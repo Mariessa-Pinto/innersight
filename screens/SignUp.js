3ad6a3d1b27f5ecbae614aa4dc9bae6a97de39a0
Mariessa-Pinto <mpinto13@my.bcit.ca>
2023-10-18 09:04:53 -0700

    sign up page created

---
+++ screens/SignUp.js
@@ -0,0 +1,50 @@
+import globalStyles from '../styles/global'
+import { StyleSheet, Text, View, Button, Switch } from 'react-native';
+import { useState, useContext } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import themeContext from '../theme/themeContext';
+import ExtraLargeBtnLightTxt from '../atom/Buttons/ExtraLargeBtnLightTxt'
+import SignUpForm from '../molecules/SignUpForm/SignUpForm';
+
+
+
+export default function SignUp({ navigation }) {
+
+    //Dark/Light Mode
+    const [darkMode, setDarkMode] = useState(false)
+    const theme = useContext(themeContext)
+
+
+    return (
+        <View style={[globalStyles.container, { backgroundColor: theme.backgroundColor }]}>
+            <StatusBar style="auto" />
+            <View style={styles.container}>
+                <View>
+                    <Text style={styles.title}>Sign Up</Text>
+                </View>
+                <SignUpForm/>
+                <ExtraLargeBtnLightTxt 
+                    text="Sign Up" 
+                    navigate="ChooseMascot"
+                />
+                <Text style={styles.account}>Already have an account?<Text style={styles.signIn}> Sign in.</Text></Text>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    signIn: {
+        fontWeight: '700',
+    },
+    title: {
+        fontSize: 28,
+        fontWeight: '700',
+        textAlign: 'left'
+    }
+})
