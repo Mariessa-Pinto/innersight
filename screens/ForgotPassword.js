d9b180ebda8e67b7968688afbc08c26be82fd04d
Mariessa-Pinto <mpinto13@my.bcit.ca>
2023-10-20 13:33:16 -0700

    added forgot password

---
+++ screens/ForgotPassword.js
@@ -0,0 +1,55 @@
+import globalStyles from '../styles/global'
+import { StyleSheet, Text, View, Button, Switch } from 'react-native';
+import { useState, useContext } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import themeContext from '../theme/themeContext';
+import ExtraLargeBtnLightTxt from '../atom/Buttons/ExtraLargeBtnLightTxt'
+import ForgotPasswordForm from '../molecules/ForgotPasswordForm/ForgotPasswordForm';
+
+
+export default function ForgotPassword() {
+
+    //Dark/Light Mode
+    const [darkMode, setDarkMode] = useState(false)
+    const theme = useContext(themeContext)
+
+    return (
+        <View style={[globalStyles.container, { backgroundColor: theme.backgroundColor }]}>
+            <StatusBar style="auto" />
+            <View style={styles.container}>
+                <View style={styles.titleContainer}>
+                    <Text style={styles.title}>Forgot Pasword?</Text>
+                    <Text>Please provide the email associated with your account below. An email with a password reset link will be sent shortly.</Text>
+                </View>
+                <ForgotPasswordForm/>
+                <ExtraLargeBtnLightTxt 
+                    text="Send Password Reset Email" 
+                    navigate="ResetPassword"
+                />
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    signIn: {
+        fontWeight: '700',
+    },
+    title: {
+        fontSize: 28,
+        fontWeight: '700',
+        textAlign: 'left'
+    },
+    titleContainer: {
+        display: "flex",
+        alignItems: 'flex-start',
+        width: 280,
+        paddingTop: 120,
+        gap: 10
+    },
+})
