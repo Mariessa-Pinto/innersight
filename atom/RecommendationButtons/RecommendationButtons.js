74d62dfd06eb9da0b330f6a6e637d312300efd16
anikagrewal <a.grewal.work@gmail.com>
2023-10-19 17:54:43 -0700

    push so i can pull

--- atom/RecommendationButtons/RecommendationButtons.js
+++ atom/RecommendationButtons/RecommendationButtons.js
@@ -1,16 +1,44 @@
-import * as React from 'react';
-import { Button } from 'react-native-paper';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableWithoutFeedback } from 'react-native';
+import { Image } from 'expo-image'
 
-const RecommendationButtons = () => (
-  <Button  mode="contained" 
-  width={328} 
-  height={106}
-  buttonColor="#3B3E75"
-  textColor='#ffffff'
-  onPress={() => console.log('Pressed')}
- 
-  > Recommendation Type
-  </Button>
-);
 
-export default RecommendationButtons;
+const RecommendationButton = () => {
+  const [pressed, setPressed] = useState(false);
+
+  return (
+    <TouchableWithoutFeedback
+      onPress={() => console.log('Pressed')}
+      onPressIn={() => setPressed(true)}
+      onPressOut={() => setPressed(false)}
+    >
+      <View style={[styles.button, pressed && styles.buttonPressed]}>
+        <Text>Text</Text>
+       <Image />
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    width: 100,
+    height: 154,
+    borderRadius: 10,
+    backgroundColor: '#F2F2FD',
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 7,
+    borderColor: '#C5C7F7',
+    borderWidth: 1
+  },
+  buttonPressed: {
+    backgroundColor: '#D5D7FF', // Change the color when pressed
+  },
+  text: {
+    color: '#3E3F42',
+    fontSize: 13,
+  },
+});
+
+export default RecommendationButton;
