2ac80aa06a0b5041c14cadd702cf92c5c850b945
Mariessa-Pinto <mpinto13@my.bcit.ca>
2023-10-20 13:33:06 -0700

    added email sent page

---
+++ screens/EmailSent.js
@@ -0,0 +1,54 @@
+import globalStyles from '../styles/global'
+import { StyleSheet, Text, View, Button, Switch } from 'react-native';
+import { useState, useContext } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import themeContext from '../theme/themeContext';
+import ExtraLargeBtnLightTxt from '../atom/Buttons/ExtraLargeBtnLightTxt'
+
+
+export default function EmailSent() {
+
+    //Dark/Light Mode
+    const [darkMode, setDarkMode] = useState(false)
+    const theme = useContext(themeContext)
+
+    return (
+        <View style={[globalStyles.container, { backgroundColor: theme.backgroundColor }]}>
+            <StatusBar style="auto" />
+            <View style={styles.container}>
+                <View style={styles.titleContainer}>
+                    <Text style={styles.title}>Email Sent</Text>
+                    <Text>Please check your email for a link to reset your password.</Text>
+                </View>
+                <ExtraLargeBtnLightTxt 
+                    text="Return to Log In" 
+                    navigate="Login"
+                />
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    signIn: {
+        fontWeight: '700',
+    },
+    title: {
+        fontSize: 28,
+        fontWeight: '700',
+        textAlign: 'left'
+    },
+    titleContainer: {
+        display: "flex",
+        alignItems: 'flex-start',
+        width: 280,
+        paddingTop: 200,
+        gap: 10,
+        paddingBottom: 40
+    },
+})
