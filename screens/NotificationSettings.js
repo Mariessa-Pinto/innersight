4cb6b3998bea8ccfbedea9b36653a2d3d08f0226
Mariessa-Pinto <mpinto13@my.bcit.ca>
2023-10-21 13:38:33 -0700

    notification page coded

--- screens/NotificationSettings.js
+++ screens/NotificationSettings.js
@@ -3,6 +3,10 @@ import { StyleSheet, Text, View, Button, Switch } from 'react-native';
 import { useState, useContext } from 'react';
 import themeContext from '../theme/themeContext';
 import NavBar from '../molecules/Navigation/NavBar';
+import Notifications from '../molecules/Notifications/Notifications';
+import Header from '../molecules/Header/Header';
+import MediumBtnLightTxt from '../atom/Buttons/MediumBtnLightTxt';
+
 
 
 
@@ -15,10 +19,23 @@ export default function NotificationSettings({ navigation }) {
 
     return (
         <View style={[globalStyles.container, { backgroundColor: theme.backgroundColor}]}>
-            <NavBar navigation={navigation} />
+            <Header title='Notifications' navigate='Profile'/>
+            <Notifications/>
+            <View style={styles.button}>
+                <MediumBtnLightTxt 
+                    text='Save Changes'
+                    navigate=''
+                />
+            </View>
+            <NavBar navigation={navigation}/>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+    button: {
+        display: 'flex',
+        alignItems: 'center',
+        paddingTop: 70
+    }
 })
