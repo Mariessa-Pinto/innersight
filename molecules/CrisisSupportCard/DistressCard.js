bc8574da8bb2f960a165eeeb565162f7f2c2c79d
anikagrewal <a.grewal.work@gmail.com>
2023-10-21 15:43:09 -0700

    crisis card & btn atom and molecule implemented

---
+++ molecules/CrisisSupportCard/DistressCard.js
@@ -0,0 +1,38 @@
+import { StyleSheet, Text, View } from 'react-native';
+import CrisisCardBtn from '../../atom/CrisisCardButton/CrisisCardBtn';
+
+const DistressCard = () => {
+    return(
+        <View style={styles.container}>
+            <CrisisCardBtn 
+            headerText="Crisis Intervention & Suicide Prevention Centre of bc"
+            numText="(604) 872-3311"
+            text="Located at 763 E Broadway, Vancouver, BC, Open 24 hours"
+            />
+            <CrisisCardBtn 
+             headerText="Crisis Intervention & Suicide Prevention Centre of bc"
+             numText="(604) 872-3311"
+             text="Located at 763 E Broadway, Vancouver, BC, Open 24 hours"
+            />
+            <CrisisCardBtn 
+             headerText="Crisis Intervention & Suicide Prevention Centre of bc"
+             numText="(604) 872-3311"
+             text="Located at 763 E Broadway, Vancouver, BC, Open 24 hours"
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+        elevation: 7,
+        marginTop: 40
+    }
+});
+
+export default DistressCard;
