dc73eb24ec81db803e88acd3318c6e414d0d57c8
Mariessa-Pinto <mpinto13@my.bcit.ca>
2023-10-21 21:17:56 -0700

    created journal tags components

---
+++ molecules/JournalTags.js/JournalTags.js
@@ -0,0 +1,89 @@
+import { StyleSheet, Text, View } from 'react-native';
+import NormalInput from '../../atom/InputBoxes/NormalInput'
+
+const JournalTags = () => {
+
+    return (
+    <View style={styles.container}>
+        <View style={styles.titleBox}>
+            <Text style={styles.title}>Journal Title</Text>
+        </View>
+        <View style={styles.topRow}>
+            <View style={styles.tagBoxBig}>
+                <Text>Tag</Text>
+            </View>
+            <View style={styles.tagBoxSmall}>
+                <Text>Tag</Text>
+            </View>
+            <View style={styles.tagBoxBig}>
+                <Text>Tag</Text>
+            </View>
+        </View>
+        <View style={styles.bottomRow}>
+            <View style={styles.tagBoxBig}>
+                <Text>Tag</Text>
+            </View>
+            <View style={styles.tagBoxBig}>
+                <Text>Tag</Text>
+            </View>
+            <View style={styles.tagBoxSmall}>
+                <Text>Tag</Text>
+            </View>
+        </View>
+    </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        width: 328,
+        height: 136, 
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#F6F6F6',
+        borderRadius: 10,
+        gap: 10,
+    },
+    topRow: {
+        display: 'flex',
+        flexDirection: 'row',
+        gap: 10
+    },
+    bottomRow: {
+        display: 'flex',
+        flexDirection: 'row',
+        gap: 10
+    },
+    tagBoxBig: {
+        width: 110,
+        height: 30,
+        backgroundColor: "#D5D7FF",
+        borderRadius: 7,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    tagBoxSmall: {
+        width: 60,
+        height: 30,
+        backgroundColor: "#D5D7FF",
+        borderRadius: 7,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    title: {
+        fontFamily: 'Lexend-Regular',
+        fontSize: 18,
+        fontWeight: '600',
+    },
+    titleBox: {
+        display: 'flex',
+        alignItems: 'flex-start',
+        width: 300
+    }
+
+});
+
+export default JournalTags;
