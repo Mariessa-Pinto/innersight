27dd098e760d270b32f0ab36b7c95c3b13383e93
Mariessa-Pinto <mpinto13@my.bcit.ca>
2023-10-30 12:51:16 -0700

    added the background on select functionality

--- molecules/ChooseMascotMol/ChooseMascotMol.js
+++ molecules/ChooseMascotMol/ChooseMascotMol.js
@@ -1,45 +1,89 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { Image } from 'expo-image';
-
+import { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 
 const ChooseMascotMol = () => {
+    const [selectedMascot, setSelectedMascot] = useState(null);
+
+    const handleMascotSelect = (mascot) => {
+        setSelectedMascot(mascot);
+    };
+
+    const mascotData = {
+        Panda: {
+            image: require('../../atom/Mascots/Panda.png'),
+            width: 113,
+            height: 108,
+            backgroundWidth: "140%",
+            backgroundHeight: "80%",
+        },
+        Sloth: {
+            image: require('../../atom/Mascots/Sloth.png'),
+            width: 108,
+            height: 101,
+            backgroundWidth: '140%',
+            backgroundHeight: '77%',
+        },
+        Otter: {
+            image: require('../../atom/Mascots/Otter.png'),
+            width: 113,
+            height: 97,
+            backgroundWidth: '140%',
+            backgroundHeight: '87%',
+        },
+        Frog: {
+            image: require('../../atom/Mascots/Frog.png'),
+            width: 107,
+            height: 88,
+            backgroundWidth: '140%',
+            backgroundHeight: '82%',
+        },
+    };
+
+    const backgroundImg = require('../../atom/Mascots/mascotBackground.png')
 
     return (
         <View style={styles.container}>
             <View style={styles.topContainer}>
-                <View style={styles.touchBox}>
-                    <Image
-                        source={require('../../atom/Mascots/Panda.png')}
-                        style={styles.panda}
-                    />
-                    <Text>Panda</Text>
-                </View>
-                <View style={styles.touchBox}>
-                    <Image
-                        source={require('../../atom/Mascots/Sloth.png')}
-                        style={styles.sloth}
-                    />
-                    <Text>Sloth</Text>
-                </View>
+                {renderMascotBox('Panda')}
+                {renderMascotBox('Sloth')}
             </View>
             <View style={styles.bottomContainer}>
-                <View style={styles.touchBox}>
-                    <Image
-                        source={require('../../atom/Mascots/Otter.png')}
-                        style={styles.otter}
-                    />
-                    <Text>Otter</Text>
-                </View>
-                <View style={styles.touchBox}>
-                    <Image
-                        source={require('../../atom/Mascots/Frog.png')}
-                        style={styles.frog}
-                    />
-                    <Text>Frog</Text>
-                </View>
+                {renderMascotBox('Otter')}
+                {renderMascotBox('Frog')}
             </View>
         </View>
     );
+
+    function renderMascotBox(mascotName) {
+        const isSelected = selectedMascot === mascotName;
+        const mascot = mascotData[mascotName];
+
+        return (
+            <TouchableOpacity
+                style={[styles.touchBox, isSelected && styles.selectedMascot]}
+                onPress={() => handleMascotSelect(mascotName)}
+            >
+                {isSelected && (
+                    <Image
+                        source={backgroundImg}
+                        style={{
+                            width: mascot.backgroundWidth,
+                            height: mascot.backgroundHeight,
+                            ...styles.backgroundImage,
+                        }}
+                    />
+                )}
+                <Image
+                    source={mascot.image}
+                    style={{
+                        width: mascot.width,
+                        height: mascot.height,
+                    }}
+                />
+                <Text>{mascotName}</Text>
+            </TouchableOpacity>
+        );
+    }
 };
 
 const styles = StyleSheet.create({
@@ -50,41 +94,33 @@ const styles = StyleSheet.create({
         gap: 40,
         marginBottom: 10,
         marginTop: 10,
-        padding: 16
+        padding: 16,
     },
-    panda: {
-        height: 128,
-        width: 130,
-    },
-    sloth: {
-        height: 128,
-        width: 136,
-    },
-    otter:{    
-        height: 128,
-        width: 148,
-    },
-    frog: {
-        height: 128,
-        width: 154,
+    backgroundImage: {
+        position: 'absolute',
+        top: 0,
+        left: -20,
     },
     topContainer: {
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'space-around',
-        width: 400
+        width: 400,
+        alignItems: 'flex-end',
     },
     bottomContainer: {
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'space-around',
-        width: 400
+        width: 400,
+        alignItems: 'flex-end',
     },
     touchBox: {
         display: 'flex',
         alignItems: 'center',
-        gap: 15
-    }
+        gap: 15,
+        position: 'relative',
+    },
 });
 
 export default ChooseMascotMol;
