739ed27bb785c1e27071007d50b2b39fe2f67b4d
Mariessa-Pinto <mpinto13@my.bcit.ca>
2023-11-04 17:29:29 -0700

    replaced image with interactive chart

--- screens/MonthStats.js
+++ screens/MonthStats.js
@@ -8,6 +8,7 @@ import 'react-native-gesture-handler';
 import Header from '../molecules/Header/Header';
 import IdentifiedEntries from '../molecules/IdentifiedEntries/IdentifiedEntries';
 import StatRecommendations from '../molecules/StatRecommendations/StatRecommendations';
+import DonutChart from '../molecules/DonutChart/DonutChart';
 
 
 export default function MonthStats({ navigation }) {
@@ -29,10 +30,7 @@ export default function MonthStats({ navigation }) {
 
                 <View style={styles.maincontent}>
                     <DateToggle selectedValue={selectedValue} />
-                    <Image
-                        source={require('../atom/assets/graph.png')}
-                        style={styles.graph}
-                    />
+                    <DonutChart />
                     <IdentifiedEntries />
                     <StatRecommendations />
                 </View>
