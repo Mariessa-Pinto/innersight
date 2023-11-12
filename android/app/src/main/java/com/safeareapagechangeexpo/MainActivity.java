9d7d58e0f9ec3a34f767679da541db2284268683
Patricia <patriciaejiang@gmail.com>
2023-11-12 15:48:16 -0800

    prebuild

--- android/app/src/main/java/com/safeareapagechangeexpo/MainActivity.java
+++ android/app/src/main/java/com/safeareapagechangeexpo/MainActivity.java
@@ -1,4 +1,4 @@
-package com.innersight.bcit;
+package com.safeareapagechangeexpo;
 
 import android.os.Build;
 import android.os.Bundle;
--- android/app/src/main/java/com/safeareapagechangeexpo/MainApplication.java
+++ android/app/src/main/java/com/safeareapagechangeexpo/MainApplication.java
@@ -1,4 +1,4 @@
-package com.innersight.bcit;
+package com.safeareapagechangeexpo;
 
 import android.app.Application;
 import android.content.res.Configuration;
