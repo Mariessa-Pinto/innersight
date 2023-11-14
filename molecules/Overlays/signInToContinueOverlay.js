3e098dc77b7ce04add02d8296fa90efc91f36ac3
Mariessa-Pinto <mpinto13@my.bcit.ca>
2023-11-13 20:42:07 -0800

    added sign up overlay

---
+++ molecules/Overlays/signInToContinueOverlay.js
@@ -0,0 +1,61 @@
+import { StyleSheet, Text, View } from 'react-native';
+import ExtraLargeBtnLightTxt from '../../atom/Buttons/ExtraLargeBtnLightTxt';
+
+const signIntoContinueOverlay = () => {
+
+    return (
+        <View style={styles.outerContainer}>
+            <View style={styles.container}>
+                <View style={styles.inside}>
+                    <View style={styles.line}></View>
+                    <Text>Please sign up to continue.</Text>
+                    <ExtraLargeBtnLightTxt 
+                        text='Sign Up'
+                        navigate='SignUp'
+                    /> 
+                </View>  
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: { 
+        width: 410,
+        height: 'auto',
+        paddingBottom: 40,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+        borderBottomLeftRadius: 0,
+        borderBottomRightRadius: 0,
+        backgroundColor: '#F2F2FD',
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: -200 },
+        shadowOpacity: 0.5,
+        shadowRadius: 3,
+        elevation: 30,
+    },
+    outerContainer: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        position: 'absolute',
+        bottom: -20,
+        left: -20
+    },
+    inside: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: 15,
+        marginTop: 10
+    },
+    line: {
+        width: 65,
+        height: 3,
+        backgroundColor: '#88898C',
+        marginBottom: 10
+    }
+
+});
+
+export default signIntoContinueOverlay;
