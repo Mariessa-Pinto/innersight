ae3c8199d44fd334d937d827540a62aeb50bb332
Mariessa-Pinto <mpinto13@my.bcit.ca>
2023-11-13 20:40:24 -0800

    adding swiper to addjournal

--- atom/JournalCovers/JournalCovers.js
+++ atom/JournalCovers/JournalCovers.js
@@ -1,11 +1,10 @@
 import globalStyles from "../../styles/global";
 import { useState, useContext } from "react";
-import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { Image } from "expo-image";
 import themeContext from '../../theme/themeContext';
 import fontContext from '../../theme/fontContext';
-
-
+import Swiper from 'react-native-swiper';
 
 const JournalCovers = () => {
 
@@ -16,23 +15,38 @@ const JournalCovers = () => {
   //Font Size
   const fontTheme = useContext(fontContext)
 
-
   return (
-
     <View style={styles.container}>
-      <ScrollView
-        horizontal={true}
-        contentContainerStyle={styles.contentContainer}>
-        <Image source={require('../assets/Covers/BlueCover.png')} style={styles.image} />
-        <View style={styles.currentJournal}>
-          <Text style={[globalStyles.h4TextLight, { color: theme.color }]}>Current</Text>
-          <Image source={require('../assets/Covers/GreenCover.png')} style={styles.image} />
+      <Text style={[globalStyles.h4TextLight, { color: theme.color }]}>Current</Text>
+      <Swiper
+        dotStyle={{
+          width: 12,
+          height: 12,
+          borderRadius: 6,
+          backgroundColor: '#DDDDDD',
+          marginTop: -850
+        }}
+        activeDotStyle={{
+          width: 12,
+          height: 12,
+          borderRadius: 6,
+          backgroundColor: '#525585',
+          marginTop: -850
+        }}
+        showsPagination loop={true}
+      >
+        <View style={styles.slide}>
+          <Image source={require('../assets/Covers/BlueCover.png')} style={styles.image} />
         </View>
-
-        <Image source={require('../assets/Covers/OrangeCover.png')} style={styles.image} />
-      </ScrollView>
-
-
+        <View style={styles.slide}>
+          <View style={styles.currentJournal}>
+            <Image source={require('../assets/Covers/GreenCover.png')} style={styles.image} />
+          </View>
+        </View>
+        <View style={styles.slide}>
+          <Image source={require('../assets/Covers/OrangeCover.png')} style={styles.image} />
+        </View>
+      </Swiper>
     </View>
   )
 };
@@ -44,7 +58,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     width: 415,
-    marginBottom: 20
+    marginBottom: -420
   },
   contentContainer: {
     display: 'flex',
@@ -59,9 +73,17 @@ const styles = StyleSheet.create({
   image: {
     width: 145.85,
     height: 219,
-    marginHorizontal: 20
+    marginHorizontal: 20,
+    borderRadius: 10
+  },
+  slide: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: -470,
+  },
+  wrapper: {
   }
-
 });
 
 export default JournalCovers;
