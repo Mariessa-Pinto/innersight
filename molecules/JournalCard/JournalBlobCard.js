e470f71933ba88bd8167a8c974585645d434e435
Mariessa-Pinto <mpinto13@my.bcit.ca>
2023-11-13 20:41:14 -0800

    adding blob animation

--- molecules/JournalCard/JournalBlobCard.js
+++ molecules/JournalCard/JournalBlobCard.js
@@ -1,15 +1,23 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
+import LottieView from 'lottie-react-native';
 
 export default function JournalBlobCard({
     imgSource,
-}) {
+    }) {
+
+    //Animation
+    const animation = require('../../atom/assets/Animations/blob_animation.json');
+
     return (
         <View style={styles.container}>
-            <Image style={styles.blobImg}
-                source={require('../../atom/assets/journalBlob.png')} />
+                <LottieView
+                            source={animation}
+                            autoPlay
+                            loop
+                            style={styles.animationStyle}
+                        />
             <Image style={styles.journalImg}
                 source={imgSource} />
-
         </View>
     )
 }
@@ -20,8 +28,10 @@ const styles = StyleSheet.create({
         height: 194,
         objectFit: 'contain',
     },
-    blobImg: {
-        position: 'absolute'
+    animationStyle: {
+        position: 'absolute',
+        width: 300,
+        top: -30
     },
     container: {
         display: 'flex',
