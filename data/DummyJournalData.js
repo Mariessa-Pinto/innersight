d6c1b603ec6303d4887b090bc739706e743fba4f
Patricia <patriciaejiang@gmail.com>
2023-11-14 12:06:33 -0800

    Add dummy data

---
+++ data/DummyJournalData.js
@@ -0,0 +1,19 @@
+const dummy = {
+    journals:[
+      {
+        content:'I feel so lazy. I have low energy. I was happy yesterday. I am tired.',
+        timestamp:Date.now(),
+        sentiments:["lazy", "low energy", "happy", "tired"]
+      },
+      {
+        content:'Today I am lazy again. I got excited and happy later though. Now I am bored.',
+        timestamp:Date.now(),
+        sentiments:["lazy", "excitement", "happy", "bored"]
+      },
+      {
+        content:'I am lazy again. I have such low energy. I am tired. ',
+        timestamp:Date.now(),
+        sentiments:["lazy", "low energy", "tired"]
+      }
+    ]
+  }
