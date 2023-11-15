a738a6d4fc3ab28ce239ff47d1f66ae86b390799
Patricia <patriciaejiang@gmail.com>
2023-11-14 17:42:24 -0800

    Mapped sentiments into chart

--- data/DummyJournalData.js
+++ data/DummyJournalData.js
@@ -5,7 +5,16 @@ export const dummy = {
         timestamp:Date.now(),
         sentiments:["lazy", "low energy", "happy", "tired"]
       },
-
+      {
+        content:'Today I am lazy again. I got excited and happy later though. Now I am bored.',
+        timestamp:Date.now(),
+        sentiments:["lazy", "excitement", "happy", "bored"]
+      },
+      {
+        content:'I am lazy again. I have such low energy. I am tired. ',
+        timestamp:Date.now(),
+        sentiments:["lazy", "low energy", "tired"]
+      }
     ]
   }
 
