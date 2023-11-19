a468b6fd2519c52d346ced5af7c671ac0608d761
anikagrewal <a.grewal.work@gmail.com>
2023-11-19 04:45:17 -0800

    keywords set up in journal and goes into chart

---
+++ firebase/firebaseKeywords.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react';
+import { Text, View } from 'react-native';
+import  { getJournalEntries } from './firebaseService'
+
+const JournalKeywordsStats = ({ username }) => {
+    const [sentsCount, setSentsCount] = useState({});
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const journals = await getJournalEntries(username);
+                processJournalEntries(journals);
+            } catch (error) {
+                console.error('Error fetching journal entries:', error);
+            }
+        };
+        fetchData();
+    }, [username]);
+
+    const processJournalEntries = (journals) => {
+        // Initalize an object to store the counts
+        const counts = {};
+
+        //Loop through each journal entry 
+        Object.values(journals).forEach(entry => {
+            entry.keywords.forEach(keyword => {
+                if (!counts[keyword]) {
+                    counts[keyword] = {
+                        count: 0,
+                        chartTitle: keyword,
+                        advice: "advice for" + keyword
+                    };
+                }
+                counts[keyword].count++;
+            });
+        });
+        setSentsCount(counts);
+    };
+
+    return (
+        <View>
+            {Object.entries(sentsCount).map(([keyword, data]) => (
+                <Text key={keyword} onPress={() => alert(data.advice)}>
+                    {keyword} - {data.count}
+                </Text>
+            ))}
+        </View>
+    );
+};
+
+export default JournalKeywordsStats;
--- firebase/firebaseService.js
+++ firebase/firebaseService.js
@@ -1,14 +1,37 @@
 import { ref, set, push, get } from 'firebase/database';
 import { database } from './firebaseConfig';
 
+const keywordsList = ['happy', 'excited', 'motivated', 'high energy', 'calm', 'relaxed', 'gratitude', 'joy', 'serenity', 'empowered', 'inspired', 'hopeful', 'love', 'bliss', 'harmony', 'courage', 'triumph', 'abundance', ' content', 'fulfilled', 'optimistic', 'healing', 'success', 'good', 'appreciation', 'growth', 'tired', 'low energy', 'unmotivated', 'lazy', 'angry', 'disappointed', 'sad', 'stressed', 'anguished', 'despair', 'frustrated', 'lonely', 'anxious', 'depressed', 'regret', 'resent', 'sorrow', 'grief', 'stress', 'confused', 'envious', 'bitter', 'rejected', 'guilty', 'irritated', 'melancholy', 'pessimistic' ]
+
 export const saveJournalEntry = async (username, entry) => {
     const journalRef = ref(database, `users/${username}/journal`);
     const newJournalEntryRef = push(journalRef);
-    await set(newJournalEntryRef, entry);
+
+    // extract keywords
+    const extractedKeywords = extractKeywords(entry.content);
+
+    await set(newJournalEntryRef, {...entry, keywords: extractedKeywords});
 };
 
+function extractKeywords(text) {
+    let extractedKeywords = [];
+    keywordsList.forEach(keyword => {
+        if (text.toLowerCase().includes(keyword.toLowerCase())) {
+            extractedKeywords.push(keyword);
+        }
+    });
+    return extractedKeywords;
+}
+
 export const getJournalEntries = async (username) => {
+    console.log(`Fetching entries for user: ${username}`);
     const journalRef = ref(database, `users/${username}/journal`);
     const snapshot = await get(journalRef);
-    return snapshot.val() || {}
+    if (snapshot.exists()) {
+        console.log("Entries found: ", snapshot.val());
+        return snapshot.val();
+    } else {
+        console.log("No entries found for user: ", username);
+        return {};
+    }
 }
