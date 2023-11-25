1c8e3a2d69d88ed41acd5eb6366474abcf4eadf2
Patricia <patriciaejiang@gmail.com>
2023-11-25 15:47:16 -0800

    nav bar dark mode added

--- atom/icons/JournalIcon.js
+++ atom/icons/JournalIcon.js
@@ -16,7 +16,7 @@ export default function JournalIcon({ icon }) {
     return (
         <>
             {
-                icon === "lightActive" ?
+                icon === "lightActive" && theme.theme === "light" ?
                     <View style={styles.container}>
                         <Svg width="100%" height="100%" viewBox="0 0 22 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <Path d="M7.73569 0.939453H19.2706C20.2256 0.939453 21.0001 1.77617 21.0001 2.80605V20.8381C21.0001 21.8689 20.2248 22.7047 19.2706 22.7047H7.73569C7.23619 22.7047 6.83105 22.2675 6.83105 21.7284V1.91577C6.83105 1.37669 7.23619 0.939453 7.73569 0.939453Z" fill="#6164C3" stroke="#EDEAEA" stroke-width="0.711639" stroke-linecap="round" stroke-linejoin="round" />
@@ -29,17 +29,52 @@ export default function JournalIcon({ icon }) {
                         </Svg>
                     </View>
                     :
-                    <View style={styles.container}>
-                        <Svg width="100%" height="100%" viewBox="0 0 22 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <Path d="M7.73569 0.939453H19.2706C20.2256 0.939453 21.0001 1.77617 21.0001 2.80605V20.8381C21.0001 21.8689 20.2248 22.7047 19.2706 22.7047H7.73569C7.23619 22.7047 6.83105 22.2675 6.83105 21.7284V1.91577C6.83105 1.37669 7.23619 0.939453 7.73569 0.939453Z" fill="#B6BAC0" stroke="#EDEAEA" stroke-width="0.711639" stroke-linecap="round" stroke-linejoin="round" />
-                            <Path d="M15.4332 0.939453H4.85501C3.86341 0.939453 3.05957 1.807 3.05957 2.87717V20.7679C3.05957 21.8381 3.86341 22.7056 4.85501 22.7056H15.4332C16.4248 22.7056 17.2286 21.8381 17.2286 20.7679V2.87717C17.2286 1.807 16.4248 0.939453 15.4332 0.939453Z" fill="#B6BAC0" stroke="#EDEAEA" stroke-width="0.711639" stroke-linecap="round" stroke-linejoin="round" />
-                            <Path d="M1.76929 11.8125H4.40916" stroke="#B6BAC0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                            <Path d="M1.76929 8.45654H4.40916" stroke="#B6BAC0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                            <Path d="M1.76929 15.1689H4.40916" stroke="#B6BAC0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                            <Path d="M1.76929 18.5244H4.40916" stroke="#B6BAC0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                            <Path d="M1.76929 5.1001H4.40916" stroke="#B6BAC0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                        </Svg>
-                    </View>
+                    <>
+                        {
+                            theme.theme === "light" ?
+                                <View style={styles.container}>
+                                    <Svg width="100%" height="100%" viewBox="0 0 22 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <Path d="M7.73569 0.939453H19.2706C20.2256 0.939453 21.0001 1.77617 21.0001 2.80605V20.8381C21.0001 21.8689 20.2248 22.7047 19.2706 22.7047H7.73569C7.23619 22.7047 6.83105 22.2675 6.83105 21.7284V1.91577C6.83105 1.37669 7.23619 0.939453 7.73569 0.939453Z" fill="#B6BAC0" stroke="#EDEAEA" stroke-width="0.711639" stroke-linecap="round" stroke-linejoin="round" />
+                                        <Path d="M15.4332 0.939453H4.85501C3.86341 0.939453 3.05957 1.807 3.05957 2.87717V20.7679C3.05957 21.8381 3.86341 22.7056 4.85501 22.7056H15.4332C16.4248 22.7056 17.2286 21.8381 17.2286 20.7679V2.87717C17.2286 1.807 16.4248 0.939453 15.4332 0.939453Z" fill="#B6BAC0" stroke="#EDEAEA" stroke-width="0.711639" stroke-linecap="round" stroke-linejoin="round" />
+                                        <Path d="M1.76929 11.8125H4.40916" stroke="#B6BAC0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                        <Path d="M1.76929 8.45654H4.40916" stroke="#B6BAC0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                        <Path d="M1.76929 15.1689H4.40916" stroke="#B6BAC0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                        <Path d="M1.76929 18.5244H4.40916" stroke="#B6BAC0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                        <Path d="M1.76929 5.1001H4.40916" stroke="#B6BAC0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                    </Svg>
+                                </View>
+                                :
+                                <>
+                                {
+                                    icon === "lightActive" && theme.theme === "dark" ?
+                                    <View style={styles.container}>
+                                    <Svg width="100%" height="100%" viewBox="0 0 22 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <Path d="M7.73569 0.939453H19.2706C20.2256 0.939453 21.0001 1.77617 21.0001 2.80605V20.8381C21.0001 21.8689 20.2248 22.7047 19.2706 22.7047H7.73569C7.23619 22.7047 6.83105 22.2675 6.83105 21.7284V1.91577C6.83105 1.37669 7.23619 0.939453 7.73569 0.939453Z" fill="#7E82F2" stroke="#2B2D31" stroke-width="0.711639" stroke-linecap="round" stroke-linejoin="round" />
+                                        <Path d="M15.4332 0.939453H4.85501C3.86341 0.939453 3.05957 1.807 3.05957 2.87717V20.7679C3.05957 21.8381 3.86341 22.7056 4.85501 22.7056H15.4332C16.4248 22.7056 17.2286 21.8381 17.2286 20.7679V2.87717C17.2286 1.807 16.4248 0.939453 15.4332 0.939453Z" fill="#7E82F2" stroke="#2B2D31" stroke-width="0.711639" stroke-linecap="round" stroke-linejoin="round" />
+                                        <Path d="M1.76929 11.8125H4.40916" stroke="#B6BAC0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                        <Path d="M1.76929 8.45654H4.40916" stroke="#B6BAC0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                        <Path d="M1.76929 15.1689H4.40916" stroke="#B6BAC0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                        <Path d="M1.76929 18.5244H4.40916" stroke="#B6BAC0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                        <Path d="M1.76929 5.1001H4.40916" stroke="#B6BAC0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                    </Svg>
+                                </View>
+                                :
+                                <View style={styles.container}>
+                                    <Svg width="100%" height="100%" viewBox="0 0 22 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <Path d="M7.73569 0.939453H19.2706C20.2256 0.939453 21.0001 1.77617 21.0001 2.80605V20.8381C21.0001 21.8689 20.2248 22.7047 19.2706 22.7047H7.73569C7.23619 22.7047 6.83105 22.2675 6.83105 21.7284V1.91577C6.83105 1.37669 7.23619 0.939453 7.73569 0.939453Z" fill="#7D8187" stroke="#2B2D31" stroke-width="0.711639" stroke-linecap="round" stroke-linejoin="round" />
+                                        <Path d="M15.4332 0.939453H4.85501C3.86341 0.939453 3.05957 1.807 3.05957 2.87717V20.7679C3.05957 21.8381 3.86341 22.7056 4.85501 22.7056H15.4332C16.4248 22.7056 17.2286 21.8381 17.2286 20.7679V2.87717C17.2286 1.807 16.4248 0.939453 15.4332 0.939453Z" fill="#7D8187" stroke="#2B2D31" stroke-width="0.711639" stroke-linecap="round" stroke-linejoin="round" />
+                                        <Path d="M1.76929 11.8125H4.40916" stroke="#7D8187" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                        <Path d="M1.76929 8.45654H4.40916" stroke="#7D8187" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                        <Path d="M1.76929 15.1689H4.40916" stroke="#7D8187" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                        <Path d="M1.76929 18.5244H4.40916" stroke="#7D8187" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                        <Path d="M1.76929 5.1001H4.40916" stroke="#7D8187" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                    </Svg>
+                                </View>
+                                }
+                                </>
+                        }
+                    </>
+
             }
         </>
 
--- atom/icons/StatsIcon.js
+++ atom/icons/StatsIcon.js
@@ -16,7 +16,7 @@ export default function StatsIcon({ icon }) {
     return (
         <>
             {
-                icon === "lightActive" ?
+                icon === "lightActive" && theme.theme === "light" ?
                     <View style={styles.container}>
                         <Svg width="100%" height="100%" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <Path d="M9.71062 0.15918H12.282C12.7625 0.15918 13.152 0.546974 13.152 1.02542V20.2892C13.152 20.8676 12.6807 21.3368 12.0999 21.3368H9.89272C9.31185 21.3368 8.84058 20.8676 8.84058 20.2892V1.02542C8.84058 0.546974 9.23007 0.15918 9.71062 0.15918Z" fill="#6164C3" stroke="#6164C3" stroke-width="0.169489" stroke-linecap="round" stroke-linejoin="round" />
@@ -25,13 +25,41 @@ export default function StatsIcon({ icon }) {
                         </Svg>
                     </View>
                     :
-                    <View style={styles.container}>
-                        <Svg width="100%" height="100%" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <Path d="M9.71062 0.15918H12.282C12.7625 0.15918 13.152 0.546974 13.152 1.02542V20.2892C13.152 20.8676 12.6807 21.3368 12.0999 21.3368H9.89272C9.31185 21.3368 8.84058 20.8676 8.84058 20.2892V1.02542C8.84058 0.546974 9.23007 0.15918 9.71062 0.15918Z" fill="#B6BAC0" stroke="#B6BAC0" stroke-width="0.169489" stroke-linecap="round" stroke-linejoin="round" />
-                            <Path d="M1.99903 9.82129H4.31324C4.8646 9.82129 5.31143 10.267 5.31143 10.8151V20.3488C5.31143 20.9272 4.84015 21.3964 4.25928 21.3964H2.05214C1.47127 21.3964 1 20.9272 1 20.3488V10.8151C1 10.2662 1.44767 9.82129 1.99819 9.82129H1.99903Z" fill="#B6BAC0" stroke="#B6BAC0" stroke-width="0.169489" stroke-linecap="round" stroke-linejoin="round" />
-                            <Path d="M17.6253 4.95361H20.0626C20.5802 4.95361 21 5.37163 21 5.88701V20.368C21 20.9296 20.5423 21.3854 19.9782 21.3854H17.7112C17.1472 21.3854 16.6895 20.9296 16.6895 20.368V5.88701C16.6895 5.37163 17.1093 4.95361 17.6269 4.95361H17.6253Z" fill="#B6BAC0" stroke="#B6BAC0" stroke-width="0.169489" stroke-linecap="round" stroke-linejoin="round" />
-                        </Svg>
-                    </View>
+                    <>
+                        {
+                            icon === "lightActive" ?
+                                <View style={styles.container}>
+                                    <Svg width="100%" height="100%" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <Path d="M9.71062 0.15918H12.282C12.7625 0.15918 13.152 0.546974 13.152 1.02542V20.2892C13.152 20.8676 12.6807 21.3368 12.0999 21.3368H9.89272C9.31185 21.3368 8.84058 20.8676 8.84058 20.2892V1.02542C8.84058 0.546974 9.23007 0.15918 9.71062 0.15918Z" fill="#B6BAC0" stroke="#B6BAC0" stroke-width="0.169489" stroke-linecap="round" stroke-linejoin="round" />
+                                        <Path d="M1.99903 9.82129H4.31324C4.8646 9.82129 5.31143 10.267 5.31143 10.8151V20.3488C5.31143 20.9272 4.84015 21.3964 4.25928 21.3964H2.05214C1.47127 21.3964 1 20.9272 1 20.3488V10.8151C1 10.2662 1.44767 9.82129 1.99819 9.82129H1.99903Z" fill="#B6BAC0" stroke="#B6BAC0" stroke-width="0.169489" stroke-linecap="round" stroke-linejoin="round" />
+                                        <Path d="M17.6253 4.95361H20.0626C20.5802 4.95361 21 5.37163 21 5.88701V20.368C21 20.9296 20.5423 21.3854 19.9782 21.3854H17.7112C17.1472 21.3854 16.6895 20.9296 16.6895 20.368V5.88701C16.6895 5.37163 17.1093 4.95361 17.6269 4.95361H17.6253Z" fill="#B6BAC0" stroke="#B6BAC0" stroke-width="0.169489" stroke-linecap="round" stroke-linejoin="round" />
+                                    </Svg>
+                                </View>
+                                :
+                                <>
+                                    {
+                                        icon === "lightActive" && theme.theme === "dark" ?
+                                            <View style={styles.container}>
+                                                <Svg width="100%" height="100%" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <Path d="M9.71062 0.15918H12.282C12.7625 0.15918 13.152 0.546974 13.152 1.02542V20.2892C13.152 20.8676 12.6807 21.3368 12.0999 21.3368H9.89272C9.31185 21.3368 8.84058 20.8676 8.84058 20.2892V1.02542C8.84058 0.546974 9.23007 0.15918 9.71062 0.15918Z" fill="#6164C3" stroke="#6164C3" stroke-width="0.169489" stroke-linecap="round" stroke-linejoin="round" />
+                                                    <Path d="M1.99903 9.82129H4.31324C4.8646 9.82129 5.31143 10.267 5.31143 10.8151V20.3488C5.31143 20.9272 4.84015 21.3964 4.25928 21.3964H2.05214C1.47127 21.3964 1 20.9272 1 20.3488V10.8151C1 10.2662 1.44767 9.82129 1.99819 9.82129H1.99903Z" fill="#6164C3" stroke="#6164C3" stroke-width="0.169489" stroke-linecap="round" stroke-linejoin="round" />
+                                                    <Path d="M17.6253 4.95361H20.0626C20.5802 4.95361 21 5.37163 21 5.88701V20.368C21 20.9296 20.5423 21.3854 19.9782 21.3854H17.7112C17.1472 21.3854 16.6895 20.9296 16.6895 20.368V5.88701C16.6895 5.37163 17.1093 4.95361 17.6269 4.95361H17.6253Z" fill="#6164C3" stroke="#6164C3" stroke-width="0.169489" stroke-linecap="round" stroke-linejoin="round" />
+                                                </Svg>
+                                            </View>
+                                            :
+                                            <View style={styles.container}>
+                                                <Svg width="100%" height="100%" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <Path d="M9.71062 0.15918H12.282C12.7625 0.15918 13.152 0.546974 13.152 1.02542V20.2892C13.152 20.8676 12.6807 21.3368 12.0999 21.3368H9.89272C9.31185 21.3368 8.84058 20.8676 8.84058 20.2892V1.02542C8.84058 0.546974 9.23007 0.15918 9.71062 0.15918Z" fill="#7D8187" stroke="#7D8187" stroke-width="0.169489" stroke-linecap="round" stroke-linejoin="round" />
+                                                    <Path d="M1.99903 9.82129H4.31324C4.8646 9.82129 5.31143 10.267 5.31143 10.8151V20.3488C5.31143 20.9272 4.84015 21.3964 4.25928 21.3964H2.05214C1.47127 21.3964 1 20.9272 1 20.3488V10.8151C1 10.2662 1.44767 9.82129 1.99819 9.82129H1.99903Z" fill="#7D8187" stroke="#7D8187" stroke-width="0.169489" stroke-linecap="round" stroke-linejoin="round" />
+                                                    <Path d="M17.6253 4.95361H20.0626C20.5802 4.95361 21 5.37163 21 5.88701V20.368C21 20.9296 20.5423 21.3854 19.9782 21.3854H17.7112C17.1472 21.3854 16.6895 20.9296 16.6895 20.368V5.88701C16.6895 5.37163 17.1093 4.95361 17.6269 4.95361H17.6253Z" fill="#7D8187" stroke="#7D8187" stroke-width="0.169489" stroke-linecap="round" stroke-linejoin="round" />
+                                                </Svg>
+                                            </View>
+                                    }
+                                </>
+                        }
+                    </>
+
+
             }
         </>
 
