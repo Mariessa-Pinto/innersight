aeb9ee3a53c4fb6f040bcff4cdd8e7ffb37bdabc
Mariessa-Pinto <mpinto13@my.bcit.ca>
2023-12-01 15:58:07 -0800

    sign in with firebase stylized

--- firebase/UserRegistration.js
+++ firebase/UserRegistration.js
@@ -1,10 +1,12 @@
+import globalStyles from '../styles/global';
+import themeContext from '../theme/themeContext';
+
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "./firebaseConfig";
-import { useState } from "react";
-import { StyleSheet, View, TextInput, Text, Button } from 'react-native';
+import { useState, useContext } from "react";
+import { StyleSheet, View, TextInput, Text, Button, TouchableOpacity } from 'react-native';
 import NormalInput from "../atom/InputBoxes/NormalInput";
-import ExtraLargeBtnLightTxt from "../atom/Buttons/ExtraLargeBtnLightTxt";
-
+import { useNavigation } from '@react-navigation/native';
 
 export default function UserRegistrationSignIn() {
     const [registerEmail, setRegisterEmail] = useState("");
@@ -14,49 +16,68 @@ export default function UserRegistrationSignIn() {
         try {
             const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword);
             console.log(user);
-        }catch(error) {
-                console.log(error);
+        } catch (error) {
+            console.log(error);
         }
     }
 
+    //Dark/Light Mode
+    const [darkMode, setDarkMode] = useState(false)
+    const theme = useContext(themeContext)
+
+    //Navigation
+    const navigation = useNavigation();
+
+    const [pressed, setPressed] = useState(false);
+
+    const navigateTo = () => {
+        navigation.navigate("ChooseMascot");
+    };
+
     return (
-    
-            <View style={styles.container}>
-             
-             <NormalInput placeholder='Name'/>
-               
-                    <TextInput 
-                        placeholder="Email"
-                        value={registerEmail}
-                        onChangeText={(text)=> {
-                            setRegisterEmail(text)
-                        }}
-                    />
-                  
-                    <TextInput 
-                        placeholder="Password"
-                        value={registerPassword}
-                        onChangeText={(pass)=> {
-                            setRegisterPassword(pass)
-                        }}
-                    />
-                    <NormalInput placeholder='Comfirm Password'/>
-                    <ExtraLargeBtnLightTxt
-                        text="Sign Up"
-                        navigate="ChooseMascot"
-                    />
-                <Button
-                style={styles.btn}
-                    title="Sign Up"
-                    onPress={() => {
-                        register()
-                        setRegisterEmail("")
-                        setRegisterPassword("")
-                        
-                }}> Sign Up </Button>
-                 
+        <View style={styles.container}>
+            <NormalInput placeholder='Name' />
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Email"
+                    value={registerEmail}
+                    onChangeText={(text) => {
+                        setRegisterEmail(text)
+                    }}
+                    keyboardType="default"
+                    blurOnSubmit={true}
+                />
             </View>
-          
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Password"
+                    value={registerPassword}
+                    onChangeText={(pass) => {
+                        setRegisterPassword(pass)
+                    }}
+                    keyboardType="default"
+                    blurOnSubmit={true}
+                />
+            </View>
+            <NormalInput placeholder='Comfirm Password' />
+            <TouchableOpacity
+                onPressIn={() => setPressed(true)}
+                onPressOut={() => setPressed(false)}
+                onPress={() => {
+                    navigateTo()
+                    register()
+                    setRegisterEmail("")
+                    setRegisterPassword("")
+                }}
+                activeOpacity={1}
+            >
+                <View style={[styles.button, pressed && styles.buttonPressed]}>
+                    <Text style={[globalStyles.btnTextLrg, styles.text]}>Sign Up</Text>
+                </View>
+            </TouchableOpacity>
+        </View>
     )
 }
 const styles = StyleSheet.create({
@@ -68,5 +89,37 @@ const styles = StyleSheet.create({
         gap: 10,
         paddingTop: 20,
         paddingBottom: 40
-    }
+    },
+    input: {
+        height: 35,
+        marginVertical: 12,
+        borderWidth: 1,
+        borderColor: '#88898C',
+        borderRadius: 10,
+        padding: 10,
+        backgroundColor: "#FDFDFD",
+        color: "#000",
+        fontWeight: 'normal',
+        fontFamily: 'Lexend-Regular'
+    },
+    inputContainer: {
+        backgroundColor: '#00000000',
+        width: '100%'
+    },
+    button: {
+        width: 228,
+        height: 44,
+        borderRadius: 10,
+        backgroundColor: '#6164C3',
+        alignItems: 'center',
+        justifyContent: 'center',
+        elevation: 7,
+        marginTop: 15,
+    },
+    buttonPressed: {
+        backgroundColor: '#43479A',
+    },
+    text: {
+        color: '#FDFDFD',
+    },
 });
--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,9 +1,8 @@
 import globalStyles from '../styles/global'
-import { StyleSheet, Text, View, Button, Switch, ScrollView } from 'react-native';
+
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import { useState, useContext } from 'react';
 import themeContext from '../theme/themeContext';
-import ExtraLargeBtnLightTxt from '../atom/Buttons/ExtraLargeBtnLightTxt'
-import SignUpForm from '../molecules/SignUpForm/SignUpForm';
 import { useNavigation } from '@react-navigation/native';
 import UserRegistrationSignIn from '../firebase/UserRegistration';
 
@@ -26,18 +25,20 @@ export default function SignUp() {
 
     return (
         <View style={[globalStyles.outerContainer, { backgroundColor: theme.backgroundGreyLight }]}>
-        <ScrollView>
-            <View style={[globalStyles.contentContainer, { backgroundColor: theme.background }]}>
-                <View style={styles.container}>
-                    <View style={styles.titleContainer}>
-                        <Text style={[globalStyles.h1TextBold, { color: theme.color }]}>Sign Up</Text>
+            <ScrollView>
+                <View style={[globalStyles.contentContainer, { backgroundColor: theme.background }]}>
+                    <View style={styles.container}>
+                        <View style={styles.titleContainer}>
+                            <Text style={[globalStyles.h1TextBold, { color: theme.color }]}>Sign Up</Text>
+                        </View>
+                        <UserRegistrationSignIn />
+                        <View style={styles.account}>
+                            <Text style={[globalStyles.bodyCopy, { color: theme.color, paddingTop: 30 }]}>Already have an account?<Text style={[globalStyles.boldBody, { color: theme.color }]} onPress={navigateTo}> Sign in.</Text></Text>
+                            <Text style={[globalStyles.bodyCopy, { color: theme.color }]}>or</Text>
+                            <Text style={[globalStyles.boldBody, { color: theme.color }]} onPress={navigateHome}>Continue as Guest</Text>
+                        </View>
                     </View>
-                    <UserRegistrationSignIn />
-                    <Text style={[globalStyles.bodyCopy, { color: theme.color, paddingTop: 30 }]}>Already have an account?<Text style={[globalStyles.boldBody, { color: theme.color }]} onPress={navigateTo}> Sign in.</Text></Text>
-                    <Text style={[globalStyles.bodyCopy, { color: theme.color }]}>or</Text>
-                    <Text style={[globalStyles.boldBody, { color: theme.color }]} onPress={navigateHome}>Continue as Guest</Text>
                 </View>
-            </View>
             </ScrollView>
         </View>
     );
@@ -48,9 +49,6 @@ const styles = StyleSheet.create({
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
-    },
-    signIn: {
-        fontWeight: '700',
     },
     title: {
         fontSize: 28,
@@ -64,9 +62,9 @@ const styles = StyleSheet.create({
         paddingTop: 70
     },
     account: {
-        paddingTop: 40
+        marginTop: -20,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
     },
-    guest: {
-        fontWeight: '500'
-    }
 })
